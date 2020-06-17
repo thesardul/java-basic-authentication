a05687b96d855d4647dccf9908e016c6e4f52325
Sardul <thesardul@users.noreply.github.com>
2020-06-17 20:45:10 +0530

    Update JDBCConstants.java

--- src/main/java/com/java/login/dao/daohelper/JDBCConstants.java
+++ src/main/java/com/java/login/dao/daohelper/JDBCConstants.java
@@ -2,7 +2,7 @@ package com.java.login.dao.daohelper;
 
 public class JDBCConstants {
     public static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
-    public static final String URL         = "jdbc:mysql://localhost/demo";
+    public static final String URL         = "jdbc:mysql://localhost/demo?serverTimezone=UTC&useSSL=false";
     public static final String USERNAME    = "root";
     public static final String PASSWORD    = "admin";
 
